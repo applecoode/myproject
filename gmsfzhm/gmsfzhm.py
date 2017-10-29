68a76e066b00bbdf71a3255253ea4517aea3d8a4
applecode <email@example.com>
2017-10-29 12:43:27 +0800

    PC-2017.2.3 <Administrator@PC Overwrite remote git@github.com:applecoode/myproject.git to local

--- gmsfzhm/gmsfzhm.py
+++
@@ -1,8 +0,0 @@
-def(sfzh):
-    if len(sfzh)!=15 and len(sfzh)!=17 and len(sfzh)!=18:
-        return {sfzh,'身份证号位数不对'}
-    elif not sfz.isalnum():
-        return {sfzh,'身份证号包含非数字字符'}
-    elif:
-        len(sfzh) = 15
-        sfzh = sfzh[:6]+'19'+sfzh[7:15]
